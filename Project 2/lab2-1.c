e87ff588a386c377cd615bddedc8e509911d245b
suarezjruben <suarezjruben@gmail.com>
2021-03-28 12:05:47 -0500

    Started project 2

--- Project 2/lab2-1.c
+++ Project 2/lab2-1.c
@@ -1,100 +1,62 @@
 /*			Lab 2 Partial Example			*/
 
-
-
 #include <stdio.h>
 
+main(){
+    char *path, *argv[20], buf[80], n, *p;
+    int m, status, inword, continu;
 
+    while(1) {
+        inword = 0;
+        p = buf;
+        m = 0;
+        continu=0;
+        printf( "\nshhh> ");
+        while ( ( n = getchar() ) != '\n'  || continu ) {
 
-main()
+            //
+            //printf("Continu %s\n", continu);
+            //printf("N: %c\n", n);
+            //
 
-{
+            if ( n ==  ' ' ) {
+                if ( inword ) {
+                    inword = 0;
+                    *p++ = 0;
+                }
+            }
+            else if ( n == '\n' ) continu = 0;
+            else if ( n == '\\' && !inword ) continu = 1;
+            else {
+                if ( !inword ) {
+                    inword = 1;
+                    argv[m++] = p;
+                    *p++ = n;
+                }
+                else *p++ = n;
+            }
 
-char *path, *argv[20], buf[80], n, *p;
+        }
 
-int m, status, inword, continu;
+        *p++ = 0;
+        argv[m] = 0;
 
+        if ( strcmp(argv[0],"exit") == 0 ) exit(0);
 
+        /* Your change starts from here */
 
-while(1) {
+        if ( fork() == 0 ){
 
+            execvp( argv[0], argv );
+            printf ( " didn't exec \n ");
 
-
-inword = 0;
-
-p = buf;
-
-m = 0;
-
-continu=0;
-
-
-
-printf( "\nshhh> ");
-
-while ( ( n = getchar() ) != '\n'  || continu ) {
-
-	if ( n ==  ' ' ) { 
-
-	    if ( inword ) {
-
-		inword = 0;
-
-		*p++ = 0;
-
-	    } 
-
-	}
-
-	else if ( n == '\n' ) continu = 0;
-
-	else if ( n == '\\' && !inword ) continu = 1;
-
-	else {
-
-	    if ( !inword ) {
-
-		 inword = 1;
-
-		 argv[m++] = p;
-
-		 *p++ = n;
-
-	     }
-
-	     else 
-
-		 *p++ = n;
-
-	}
-
-}	
-
-*p++ = 0;
-
-argv[m] = 0;
+        }
 
 
 
-if ( strcmp(argv[0],"exit") == 0 ) exit (0);
+        wait(&status);
 
-/* Your change starts from here */
-
-if ( fork() == 0 )
-
-	{
-
-	execvp( argv[0], argv );
-
-	printf ( " didn't exec \n ");
-
-	}
-
-
-
-wait(&status);
-
-}
+    }
 
 }
 
